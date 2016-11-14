9953770d65a280f6e41fa9138c9120b3491dbbe0
Ian Thorp <IanMThorp@gmail.com>
2016-11-14 15:54:49 -0800

    Display basic page

--- js/BrowserEntry.jsx
+++ js/BrowserEntry.jsx
@@ -1,13 +1,5 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const ReactRouter = require('react-router')
-// const App = require('')
+const App = require('./Landing')
 
-const { match } = ReactRouter
-
-match({ history: App.History, routes: App.Routes }, (error, redirectLocation, renderProps) => {
-  if (error) {
-    return console.error('BrowserEntry require.ensure error', error)
-  }
-  ReactDOM.render(<App {...renderProps} />, document.getElementById('app'))
-})
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ js/Landing.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+const App = React.createClass({
+  render () {
+    return (
+      <header className='header'>
+        <h1 className='branding'>
+          Trello Mailer
+        </h1>
+      </header>
+    )
+  }
+})
+
+module.exports = App
